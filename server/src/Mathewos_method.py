f10c4eed948e0a2dbbfd217985473cac32380fb3
Valdemar Rydberg <valdemar.ryd@gmail.com>
2025-09-16 17:25:45 +0200

    added more methods

---
+++ server/src/Mathewos_method.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from typing import Optional, Iterable
+import hmac, hashlib
+import fitz  # PyMuPDF
+from watermarking_method import (
+    WatermarkingMethod,
+    PdfSource,
+    load_pdf_bytes,
+    SecretNotFoundError,
+    InvalidKeyError,
+    WatermarkingError,
+)
+
+class MathewosMethod(WatermarkingMethod):
+    name = "Mathewos"
+    description = "Explain waht your method does here"
